19cdf640792419e32e6b2a016b0a5b2af8d305ac
Danven Ong <danvenongwd@gmail.com>
2024-03-31 19:34:53 +0800

    Added Operators

---
+++ src/Operators/AbbreviatingOperators.java
@@ -0,0 +1,24 @@
+package Operators;
+
+public class AbbreviatingOperators {
+    public static void main(String[] args) {
+
+        //Post-fix Increment Operator
+        int result = 100;
+        result++; //This also means result = result + 1;
+        result--; //This means result = result - 1;
+
+        //Compound Operator Assignment (supports +, -, *, /)
+        result += 1; //This means result = result + 1;
+        result -= 5; //This means result = result - 5;
+
+        int otherResult = 10;
+        otherResult -= 5.5;
+        System.out.println(otherResult); //4, using Compound Operator Assignment hides the lossy conversion error
+
+        int otherResult2 = 10;
+        otherResult2 = (int) (otherResult2 - 5.5);
+        System.out.println(otherResult2); //4, this also fixes the lossy conversion error using casting
+        //Conclusion: x -= y is x = (data type of x) (x - y)
+    }
+}
---
+++ src/Operators/BasicOperators.java
@@ -0,0 +1,25 @@
+package Operators;
+
+public class BasicOperators {
+    public static void main(String[] args) {
+
+        //e.g. of Operators on numbers
+        int result = 1 + 2; //result = 3
+        result = result - 2; //result = 1
+
+        //Remainder operator %
+        System.out.println(10 % 5); //0
+
+        //Using % to test for even numbers, when the final result == 0
+        System.out.println(10 % 2); //0
+        System.out.println(9 % 2); //1
+
+
+        //+ Operator on char
+        char firstChar ='A', secondChar = 'B';
+        System.out.println(firstChar + secondChar); //131, numbers in memory get added together instead if the characters
+        System.out.println("" + firstChar + secondChar); //AB, concat with string to get string result
+
+
+    }
+}
